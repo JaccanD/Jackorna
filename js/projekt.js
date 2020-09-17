a6b6edf4c47a0420212e21d8681153ba2147c933
JaccanD <63229159+JaccanD@users.noreply.github.com>
2020-09-17 14:22:11 +0200

    ttt

--- js/projekt.js
+++ js/projekt.js
@@ -3,23 +3,36 @@ init = function() {
     var canvas = document.getElementById('mainCanvas');
     var context = canvas.getContext('2d');
 
-    var SquatButton = new CanvasButton(0, 100, 0, 100);
+    var SquatButton = new CanvasButton(400, 500, 100, 600);
     var pushupButton = new CanvasButton(0, 10, 0, 10);
     var shoulderPressButton = new CanvasButton(0, 20, 0, 20);
     var bicepsCurlButton = new CanvasButton(0, 30, 0, 30);
     var sittupButton = new CanvasButton(0, 40, 0, 40);
     var dumbBellButton = new CanvasButton(0, 50, 0, 50);
 
-    var mainScreenButtons = { SquatButton, pushupButton, shoulderPressButton, bicepsCurlButton, sittupButton, dumbBellButton };
-
-    processClick = function(event) {
-        if (SquatButton.checkClick(event.x, event.y)) {
-            console.log("Tryckte på knappen");
-        }
+    var mainScreenButtons = [SquatButton, pushupButton, shoulderPressButton, bicepsCurlButton, sittupButton, dumbBellButton];
+    var quizScreen = [];
+    switchScreen = function(from, destination) {
+        console.log("Byter skärm::");
+        for (var i = 0; from.length; i++) {
+            from[i].active = false;
+    }
+    for (i = 0; destination.length; i++) {
+        destination[i].active = true;
     }
 
-    // Kod som körs
-    canvas.addEventListener("click", processClick);
+}
+processClick = function(event) {
+    if (mainScreenButtons[0].checkClick(event.x, event.y)) {
+        console.log("Kommer in i processClick");
+        switchScreen(mainScreenButtons, quizScreen);
+    } else {
+        console.log("Inte på en knapp");
+    }
+}
+
+// Kod som körs
+canvas.addEventListener("click", processClick);
 }
 
 // Klasser
@@ -44,4 +57,11 @@ class CanvasButton {
         return false;
     }
 }
+class Quiz {
+    constructor(questions, answers) {
+        this.questions = questions;
+        this.answers = answers;
+    }
+}
+
 window.addEventListener("load", init);
