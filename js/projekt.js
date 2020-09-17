2fe3a2bc80bb8b54ede9ad799dc3ac635ac52d72
JaccanD <63229159+JaccanD@users.noreply.github.com>
2020-09-17 11:14:32 +0200

    knapp klass i javascriptet

--- js/projekt.js
+++ js/projekt.js
@@ -1,9 +1,37 @@
-var canvas = document.getElementById('mainCanvas');
-var context = canvas.getContext('2d');
+init = function() {
+    var canvas = document.getElementById('mainCanvas');
+    var context = canvas.getContext('2d');
 
-context.beginPath();
-context.moveTo(80, 280);
-context.lineTo(360, 50);
-context.lineWidth = 10;
-context.strokeStyle = '#aa00aa';
-context.stroke();
+
+    var SquatButton = new CanvasButton(0, 1, 0, 1);
+    alert(SquatButton.xEnd);
+    context.beginPath();
+    context.moveTo(80, 280);
+    context.lineTo(360, 50);
+    context.lineWidth = 10;
+    context.strokeStyle = '#aa00aa';
+    context.stroke();
+
+    processClick = function(event) {
+
+    }
+    canvas.addEventListener("click", processClick);
+}
+class CanvasButton {
+    constructor(xStart, xEnd, yStart, yEnd) {
+        this.xStart = xStart;
+        this.xEnd = xEnd;
+        this.yStart = yStart;
+        this.yEnd = yEnd;
+    }
+
+    checkClick(x, y) {
+        if (xStart <= x && xEnd >= x) {
+            if (yStart <= y && yEnd >= y) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+window.addEventListener("load", init);
